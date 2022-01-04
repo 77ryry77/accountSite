85576a5493152b5bd49ae5271459a05dddb1f60b
unknown <ryanadalal@gmail.com>
2022-01-03 22:11:58 -0500

    login and create works and create checks for emails/usernames in use and password matches

---
+++ README.md
@@ -0,0 +1,5 @@
+to view use xaamp and go to http://localhost/test/login/
+
+to view account list go to http://localhost/phpmyadmin/
+
+this site is not safe to actually use
--- create/create.php
+++ create/create.php
@@ -17,51 +17,57 @@
         $conn = new mysqli($servername, $susername, $spassword, $dbname);
         // Check connection
         if ($conn->connect_error) {
-        die("Connection failed: " . $conn->connect_error);
+            die("Connection failed: " . $conn->connect_error);
         }
 
         $sql = "SELECT id, uemail, uname FROM accounts";
         $result = $conn->query($sql);
-
-
-        $success = FALSE;
+        /*
+        $i = 0;
+        while ($i < 3){
+            $row = $result->fetch_array(MYSQLI_BOTH);
+            echo $row["uemail"];
+            $i++;
+        }*/
         //user info validation makes sure the passwords match, the username is available and the email is available
         if ($result->num_rows > 0) {
             // output data of each row
-            while($row = $result->fetch_assoc()) {
+            $success = FALSE;
+            for($i = 0; $i < $result->num_rows; $i++) {
+                echo $i;
+                $row = $result->fetch_array(MYSQLI_BOTH);
                 //checks to make sure the username and email are not already in use
+                echo $row["uemail"];
+                echo $email;
                 if($row["uemail"] == $email)
                 {
-                    header("Location: failedcreateinuse.html");
+                    header("Location: failedcreateinuse.php");
+                    $success = TRUE;
                 }
                 elseif($row["uname"] == $username)
                 {
-                    header("Location: failedcreateinuse.html");
+                    header("Location: failedcreateinuse.php");
+                    $success = TRUE;
                 }
                 //checks to make sure the passwords are the same
                 elseif($password != $passwordverify)
                 {
-                    header("Location: failedcreatepassword.html");
-                }
-                else{
+                    header("Location: failedcreatepassword.php");
                     $success = TRUE;
-                    break;
                 }
             }
-            if($success == TRUE)
-            {
+            if (!$success){
                 // sql to add new user to the database
                 $sql = "INSERT INTO accounts (uname, ufname, ulname, uemail, upassword)
                 VALUES ('$username', '$fname', '$lname', '$email', '$password')";
                 
                 if ($conn->query($sql) === TRUE) {
-                echo "New account created successfully";
+                    echo "New account created successfully";
+                    echo "<br /><a href='../login/login.html'>Login</a>";
                 } else {
-                echo "Error: " . $sql . "<br>" . $conn->error;
+                    echo "Error: " . $sql . "<br>" . $conn->error;
                 }
-                echo "<br /><a href='login.html'>Login</a>";
             }
-
         } else {
             echo "Error";
         }
